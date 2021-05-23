ae7e3c4e5f95cb14ba08a572258903f4261d7419
mykolasenyuk <73036879+mykolasenyuk@users.noreply.github.com>
2021-05-23 23:05:15 +0100

    fixed btn

--- src/js/asyncOnSearch.js
+++ src/js/asyncOnSearch.js
@@ -6,6 +6,7 @@ import { error, notice } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import * as basicLightbox from 'basiclightbox'
+import LoadMoreBtn from './loadMoreBtn'
 
 
 
@@ -13,14 +14,18 @@ const refs = {
     searchInput: document.querySelector('.search-form'),
     loadMoreBtn: document.querySelector('[data-action="load-more"]'),
     imgsList: document.querySelector('.gallery')
-
 }
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '[data-action="load-more"]',
+    hidden: true
+})
+// console.log(loadMoreBtn)
 const apiService = new ApiService();
 // console.log(apiService)
 
 refs.searchInput.addEventListener('submit', onSearch);
-refs.loadMoreBtn.addEventListener('click',onLoadMore)
-
+loadMoreBtn.refs.button.addEventListener('click',onLoadMore)
+// refs.loadMoreBtn.addEventListener('click',onLoadMore)
 
 
 
@@ -34,15 +39,17 @@ refs.loadMoreBtn.addEventListener('click',onLoadMore)
 
     if (e.currentTarget.elements.query.value[0] === ' '
         || e.currentTarget.elements.query.value === '') {
-     
        return   notice({
            text: 'Please,enter valid name'
        })   
      }
+
      apiService.resetPage()
+      loadMoreBtn.show()
+         loadMoreBtn.disable()
 
      const result = await apiService.fetchImages(apiService.searchQuery)
-    //  console.log(result)
+    
      if (result.length === 0) {
           error({
               text: `'${apiService.searchQuery}' not found!
@@ -50,45 +57,43 @@ refs.loadMoreBtn.addEventListener('click',onLoadMore)
        })   
      }
      try {
-        clearImgsMarkup();
-         appendImgsMarkup(result)
+        
+         clearImgsMarkup();
+         appendImgsMarkup(result);
+         loadMoreBtn.enable()
           window.scrollTo({
   top: 150,
   behavior: 'smooth'
 });   
-    
      } catch (error) {
          notice({
            text: 'Please,enter valid name'
-       })
-          
+       })     
 }
-
 };
 
 
 async function onLoadMore() {
+    loadMoreBtn.disable();
     const result = await apiService.fetchImages(apiService.searchQuery);
 try {
-    appendImgsMarkup(result)
+    appendImgsMarkup(result);
+    loadMoreBtn.enable();
     
 } catch (error) {
     notice({
         text: 'Please,enter valid name'
     })
 }
- 
-}
+};
 
 function appendImgsMarkup(hits) {
     refs.imgsList.insertAdjacentHTML('beforeend', imgsTmpl(hits))
-    
-}
+};
 
 function clearImgsMarkup() {
-    refs.imgsList.innerHTML = '';
-    
-}
+    refs.imgsList.innerHTML = '';  
+};
 
 
 // const instance = basicLightbox.create(`
